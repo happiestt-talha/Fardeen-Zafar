eb3e653359821b139e8514daafa427c93e67b9ba
Talha <mtalhamanzoor786@gmail.com>
2024-07-15 21:10:52 +0500

    working

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,23 +3,49 @@ import styled from 'styled-components'
 
 const FooterContainer = styled.div`
   width: 100%;
-  height: 3rem;
+  /* height: 3rem; */
   background-color: ${({ theme }) => theme.colors.tertiary};
   /* color: ${({ theme }) => theme.colors.primary}; */
   color: white;
-  padding: 0.5rem 1rem;
+  padding: 1rem 1rem;
   display: flex;
+  flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
   font-family: ${({ theme }) => theme.fonts.secondary};
   font-size: 1.5rem;
+
+`
+
+const HeartPara = styled.p`
+  font-size: 1.5rem;
+  display: flex;
+  align-items: center;
+
+  gap: 0.5rem;
+
+  a{
+    line-height: 1.5rem;
+    text-decoration: underline;
+    color: inherit;
+  }
+  &:hover :last-child{
+    color: red;
+    transform: scale(1.5);
+  }
+`
+
+const Heart=styled.p`
+  color: black;
+  cursor: pointer;
+  transition: all .5s;
 `
 const Footer = () => {
   return (
     <>
       <FooterContainer>
         <p>Copyright &copy; {new Date().getFullYear()}</p>
-        <p>Created by DEVED with love <span role="img" style={{ fontSize: '2rem', color: 'red' }}>&hearts;</span> </p>
+        <HeartPara>Created by <a href='https://www.linkedin.com/in/mt4lha' target='_blank' rel='noreferrer'>DEVED</a> with love <Heart>&hearts;</Heart> </HeartPara>
       </FooterContainer>
     </>
   )
