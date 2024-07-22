5bae678b5298d13ed209f8fc4e271648cf4b14d7
Talha <mtalhamanzoor786@gmail.com>
2024-07-22 13:38:12 +0500

    Completed Cards

--- client/src/components/ContactCard.jsx
+++ client/src/components/ContactCard.jsx
@@ -1,63 +1,87 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-
+import { FaInstagram, FaInstagramSquare } from "react-icons/fa";
 const CardContainer = styled.div`
-  width: 20rem;
-  height: 20rem;
-  border: 2px solid #ccc;
-  border-radius: 1.2rem;
-  position: relative;
-  perspective: 1000px;
-  transform-style: preserve-3d;
-  transition: transform 0.6s;
+    width: 20rem;
+    height: 20rem;
+    border: 2px solid #ccc;
+    border-radius: 1.2rem;
+    position: relative;
+    perspective: 1000px;
+    transform-style: preserve-3d;
+    transition: transform 0.9s;
 
-  &:hover {
+    &:hover {
     transform: rotateY(180deg);
-  }
+    }
 `;
 
 const Face = styled.div`
-  height: 100%;
-  width: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  backface-visibility: hidden;
-  border-radius: 1.2rem;
-  backdrop-filter: blur(25px); /* Apply the blur effect */
-  -webkit-backdrop-filter: blur(25px); /* For Safari support */
+    height: 100%;
+    width: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    backface-visibility: hidden;
+    border-radius: 1.2rem;
+    backdrop-filter: blur(25px); /* Apply the blur effect */
+    -webkit-backdrop-filter: blur(25px); /* For Safari support */
 `;
 
 const FrontFace = styled(Face)`
-  /* background-color: rgba(255, 255, 255, 0.8);  */
-  color: #000;
+    color: #000;
 `;
 
 const BackFace = styled(Face)`
-  /* background-color: rgba(141, 192, 237, 0.8);  */
-  color: #000;
-  transform: rotateY(180deg);
+    color: #000;
+    transform: rotateY(180deg);
+    /* background:linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%) ; */
 `;
 
-const ContactCard = () => {
-  return (
-    <CardContainer>
-      <FrontFace>
-        <Link to='https://www.linkedin.com/in/fardeen-zafar/' target='_blank' rel='noreferrer' style={{ textDecoration: 'none', color: 'inherit' }}>
-          <h1>Front Face</h1>
-        </Link>
-      </FrontFace>
-      <BackFace>
-        <Link to='https://www.linkedin.com/in/fardeen-zafar/' target='_blank' rel='noreferrer' style={{ textDecoration: 'none', color: 'inherit' }}>
-          <h1>Back Face</h1>
-        </Link>
-      </BackFace>
-    </CardContainer>
-  );
+const FrontIcon = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 13rem;
+    color: ${({ theme }) => theme.colors.light};
+    margin-bottom: 1rem;
+`
+const BackIcon = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 1rem;
+    font-size: 3rem;
+
+    background:linear-gradient(45deg, #f09433 0%, #e6683c 25%, #dc2743 50%, #cc2366 75%, #bc1888 100%) ;
+    background-clip: text;
+    -webkit-background-clip: text;
+    color: transparent;
+`
+const ContactCard = (props) => {
+    return (
+        <CardContainer>
+            <FrontFace>
+                <Link to='https://www.linkedin.com/in/fardeen-zafar/' target='_blank' rel='noreferrer' style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <FrontIcon>{props.Ficon}</FrontIcon>
+                </Link>
+            </FrontFace>
+            <BackFace>
+                <Link to='https://www.linkedin.com/in/fardeen-zafar/' target='_blank' rel='noreferrer' style={{ textDecoration: 'none', color: 'inherit' }}>
+                    <BackIcon>
+                        <p>
+                            {props.Bicon}
+                        </p>
+                        
+                    </BackIcon>
+                </Link>
+            </BackFace>
+        </CardContainer>
+    );
 };
 
 export default ContactCard;
--- client/src/pages/ContactPage.jsx
+++ client/src/pages/ContactPage.jsx
@@ -1,26 +1,60 @@
 import React from 'react'
 import ContactCard from '../components/ContactCard'
 import styled from 'styled-components'
+import contactBg from '../assets/images/contact-bg.jpg'
+import { FaInstagram, FaInstagramSquare, FaLinkedinIn, FaTwitterSquare } from "react-icons/fa";
 
 const ContactContainer = styled.div`
     height: calc(100vh - 11.5rem);
-
+    width: 100%;
     display: flex;
     justify-content: center;
     align-items: center;
 
     gap: 1rem;
 
-    background: url('https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1');
+    /* background: url('https://images.pexels.com/photos/574071/pexels-photo-574071.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1'); */
 
+    background-image: url(${contactBg});
+    background-size: cover;
+    background-position: center;
+
+    @media (max-width: 768px) {
+        flex-direction: column;
+        height: max-content;
+    }
 `
+
 const ContactPage = () => {
+    const data = [
+        {
+            id: 1,
+            Ficon: <FaInstagramSquare />,
+            Bicon: 'Card 1'
+        },
+        {
+            id: 2,
+            Ficon: <FaTwitterSquare />,
+            Bicon: 'Card 2'
+        },
+        {
+            id: 3,
+            Ficon: <FaLinkedinIn />,
+            Bicon: 'Card 3'
+        }
+    ]
     return (
         <>
             <ContactContainer>
-                <ContactCard />
-                <ContactCard />
-                <ContactCard />
+                {
+                    data.map((item) => {
+                        return <ContactCard
+                            key={item.id}
+                            Ficon={item.Ficon}
+                            Bicon={item.Bicon}
+                        />
+                    })
+                }
             </ContactContainer>
         </>
     )
